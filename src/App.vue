30af74a9e4fd1b2560df67a68aff709268c27b01
stevelan <lanyp@qlmtalent.com>
2019-08-12 23:38:59 +0800

    app is now able to fetch questions and display one page of its content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
+    <QuestionBox 
+      :current_question="question_list[index]"
+    />
   </div>
 </template>
 
@@ -10,14 +12,34 @@
 import Header from './components/Header.vue'
 import QuestionBox from './components/QuestionBox.vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'app',
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      question_list: [],
+      index: 0,
+      total_questions: 0
+    }
+  },
+  methods: {
+    next() {
+      if (this.index < this.total_questions - 1) {
+        this.index++
+      }
+    }
+  },
+  mounted: function() {
+    this.axios.get(
+      'https://opentdb.com/api.php?amount=10'
+    ).then((response) => {
+      this.question_list = response.data.results
+      this.total_questions = response.data.results.length
+    })
   }
 }
 </script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -5,13 +5,16 @@
         <b-col cols="6">
           <div class="mt-3"></div>
           <b-jumbotron>
-            <template slot="lead">Question Title</template>
+            <template slot="lead">{{ current_question.question }}</template>
             <hr class="my-2">
             <b-list-group>
-              <b-list-group-item>Cras justo odio</b-list-group-item>
-              <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
-              <b-list-group-item>Morbi leo risus</b-list-group-item>
-              <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+              <b-list-group-item
+                v-for="(answer, index) in answers"
+                :key="index"
+
+              >
+                {{ answer }}
+              </b-list-group-item>
             </b-list-group>
             <hr class="my-3">
             <b-button variant="primary" href="#">Submit</b-button>
@@ -24,7 +27,20 @@
 </template>
 
 <script>
-  
+  import _ from 'lodash'
+
+  export default {
+    props: {
+      current_question: Object
+    },
+    computed: {
+      answers() {
+        let answers = [...this.current_question.incorrect_answers]
+        answers.push(this.current_question.correct_answer)
+        return answers
+      }
+    }
+  }
 </script>
 
 <style>

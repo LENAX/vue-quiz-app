ff3c041e8c2a8b20e45199877deb53d52d0924ab
stevelan <lanyp@qlmtalent.com>
2019-08-14 23:53:34 +0800

    add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row class="justify-content-md-center">
+        <b-col cols="6">
+          <div class="mt-3"></div>
+          <b-jumbotron>
+            <template>
+              <b-row class="justify-content-md-center">
+                <b-col cols="12">
+                  <h2>Questions Loading ... </h2>
+                </b-col>
+              </b-row>
+              <b-row class="justify-content-md-center">
+                <b-col cols="12">
+                  <b-spinner label="Loading..."></b-spinner>
+                </b-col>
+              </b-row>
+            </template>
+          </b-jumbotron>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
